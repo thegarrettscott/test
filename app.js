4b799fc199e1e3a6fdd06937c6b12aaef4ddeb58
thegarrettscott <78319808+thegarrettscott@users.noreply.github.com>
2024-03-01 21:50:35 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Our Veterinary Clinic</h1>
+        <p>Your pets' health is our top priority.</p>
+        <a
+          className="App-link"
+          href="/services"
+          rel="noopener noreferrer"
+        >
+          View Our Services
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
